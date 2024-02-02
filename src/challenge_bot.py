f71cebcddb6d6a857bb25808d5ed2101f4bf0d57
Eric Wolff <ericwolff42@gmail.com>
2024-02-02 15:24:29 -0500

    Fixed bot challenge so it plays the correct format

--- src/challenge_bot.py
+++ src/challenge_bot.py
@@ -2,22 +2,27 @@ import asyncio
 
 from poke_env import AccountConfiguration
 
-from src.poke_env_classes import TrainedRLPlayer
+from src.poke_env_classes import TrainedRLPlayer, MultiTeambuilder
+from src.utils.general import repo_root, get_packed_teams
 
 
-async def challenge_human(player: TrainedRLPlayer, opponent:str, n_battles:int=1):
+async def challenge_human(player: TrainedRLPlayer, opponent: str, n_battles: int = 1):
     await player.send_challenges(opponent, n_battles)
 
 
 if __name__ == '__main__':
-    model_path = r"C:\Users\Eric\Desktop\proj\CSSeniorProject\checkpoints\240131_TestRun\player_1\PokeNet_33.chkpt"
+    model_path = r"C:\Users\Eric\Desktop\proj\CSSeniorProject\checkpoints\2402001_1mil_Resumed\player_1\PokeNet_149.pt"
     opponent_username = "stankyBagel42"
     loop = asyncio.get_event_loop()
 
     bot_account = AccountConfiguration("RL Trained Bot", None)
+
+    teams = MultiTeambuilder(get_packed_teams(repo_root / 'packed_teams'))
     pokemon_bot = TrainedRLPlayer(
         model=model_path,
         account_configuration=bot_account,
+        battle_format='gen4anythinggoes',
+        team=teams
     )
 
     loop.run_until_complete(challenge_human(pokemon_bot, opponent_username))
